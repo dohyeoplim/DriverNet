dc89d2f0ce42f56593b64bfc44942ab12edb12b9
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-07 08:05:11 +0900

    fix: update datamodule checker

--- scripts/test_datamodule.py
+++ scripts/test_datamodule.py
@@ -2,9 +2,14 @@ from src.DriverNet.data.DataModule import DriverDataModule
 
 dm = DriverDataModule(
     data_dir="./input/imgs/train",
+    csv_path="./input/driver_imgs_list.csv",
     batch_size=8,
     num_workers=0,
+    pin_memory=False,
+    persistent_workers=False,
     image_size=224,
+    flip_p=0.1,
+    test_split=0.2,
 )
 
 dm.prepare_data()
