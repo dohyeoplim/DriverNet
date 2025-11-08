5eed77f24119c878080afd318a3f019919a07eb6
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-08 15:02:01 +0900

    feat: add wandb

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import argparse
 from pathlib import Path
+import wandb
 
 def parse_args() -> argparse.Namespace:
     p = argparse.ArgumentParser(description="DriverNet")
     p.add_argument("--download-dataset", action="store_true", help="Download dataset from Kaggle")
-    p.add_argument("--train", default="teacher", help="Train [teacher/student] model")
+    p.add_argument("--train", choices=["teacher", "student"], help="Train [teacher/student] model")
     return p.parse_args()
 
 def main():
+    wandb.init(project="DriverNet", entity="dohyeoplim-edu")
+
     args = parse_args()
     if args.download_dataset:
         from src.DriverNet.utils.download_dataset import download_kaggle_competition
