0f3fe7fed52625b59cd2c7a92c13cacfa7e7619a
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-08 17:18:12 +0900

    feat: use log model all

--- src/DriverNet/utils/logger.py
+++ src/DriverNet/utils/logger.py
@@ -1,3 +1,3 @@
 from lightning.pytorch.loggers import WandbLogger
 
-wandb_logger = WandbLogger(project="DriverNet", entity="dohyeoplim-edu")
+wandb_logger = WandbLogger(project="DriverNet", entity="dohyeoplim-edu", log_model="all")
