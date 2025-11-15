0ef3362bc58d1c30eac80c36a9fd45cea62b1aa5
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-15 23:20:38 +0900

    conf: update params

--- src/DriverNet/core/train.py
+++ src/DriverNet/core/train.py
@@ -19,7 +19,7 @@ def _build_callbacks(monitor: str, mode: str, prefix: str):
     earlyStopping = EarlyStopping(
         monitor=monitor,
         mode=mode,
-        patience=4,
+        patience=7,
         min_delta=1e-4,
         check_finite=True,
         verbose=True,
