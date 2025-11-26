dabb80906276ed7e3cb4e3707c43100fe9b94719
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-26 05:32:19 +0000

    feat: enhance dgap head

--- src/DriverNet/models/heads/DepthGroupedHead/core.py
+++ src/DriverNet/models/heads/DepthGroupedHead/core.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from src.DriverNet.utils.depth_utils import compute_depth_groups, masked_avg_pool
 
 def depth_group_pooled_features(feat: torch.Tensor, depth: torch.Tensor) -> torch.Tensor:
-    B, C, Hf, Wf = feat.shape
+    # B, C, Hf, Wf = feat.shape
 
     depth_r, q1, q2 = compute_depth_groups(depth, feat)
     mask_near = depth_r <= q1
@@ -28,19 +28,33 @@ class DepthGroupedHead(nn.Module):
     ):
         super().__init__()
         in_dim = feat_dim * 4
-        hidden = in_dim
+        hidden1 = in_dim // 2
+        hidden2 = hidden1 // 2
 
         self.pre_norm = nn.LayerNorm(in_dim)
-        self.fc1 = nn.Linear(in_dim, hidden)
-        self.act = nn.SiLU()
-        self.dropout = nn.Dropout(dropout)
-        self.fc_out = nn.Linear(hidden, num_classes)
+
+        self.fc1 = nn.Linear(in_dim, hidden1)
+        self.act1 = nn.SiLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.act2 = nn.SiLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.fc_out = nn.Linear(hidden2, num_classes)
 
     def forward(self, feat: torch.Tensor, depth: torch.Tensor) -> torch.Tensor:
         pooled = depth_group_pooled_features(feat, depth)
         x = self.pre_norm(pooled)
+
         x = self.fc1(x)
-        x = self.act(x)
-        x = self.dropout(x)
+        x = self.act1(x)
+        x = self.dropout1(x)
+
+        x = self.fc2(x)
+        x = self.act2(x)
+        x = self.dropout2(x)
+
         logits = self.fc_out(x)
+
         return logits
